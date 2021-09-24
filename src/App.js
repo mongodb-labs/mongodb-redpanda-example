0550083ec96103f118663b959ae1ede818918ffd
Rob Walters <Robert.Walters@mongodb.com>
2021-09-24 14:15:41 -0400

    intial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Wack-a-Table placeholder
         </p>
         <a
           className="App-link"
